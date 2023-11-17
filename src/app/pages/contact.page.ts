387e3d2750da94da6a7315f77a459b75545f8bc2
Derlys Dominguez <derlys275@gmail.com>
2023-11-17 16:32:43 +0000

    add: routes children

---
+++ src/app/pages/contact.page.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-contact',
+    standalone: true,
+    template:`
+
+    <h1>Contacto</h1>
+    
+    `
+})
+
+export default class ContactPage{
+
+}
